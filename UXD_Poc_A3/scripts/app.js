a01387a0edb1f901dfba061fa9654bb0ed590f0f
unseen1980 <koutsiaris@gmail.com>
2017-01-20 23:46:43 +0000

    added eshop

--- UXD_Poc_A3/scripts/app.js
+++ UXD_Poc_A3/scripts/app.js
@@ -10,48 +10,50 @@ function closeNav() {
 
 $(function() {
 
-    // function getTimeRemaining(endtime) {
-    //     var t = Date.parse(endtime) - Date.parse(new Date());
-    //     var seconds = Math.floor((t / 1000) % 60);
-    //     var minutes = Math.floor((t / 1000 / 60) % 60);
-    //     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    //     var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    //     return {
-    //         'total': t,
-    //         'days': days,
-    //         'hours': hours,
-    //         'minutes': minutes,
-    //         'seconds': seconds
-    //     };
-    // }
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 
 
-    // function initializeClock(id, endtime) {
-    //     var clock = document.getElementById(id);
-    //     var daysSpan = clock.querySelector('.days');
-    //     var hoursSpan = clock.querySelector('.hours');
-    //     var minutesSpan = clock.querySelector('.minutes');
-    //     var secondsSpan = clock.querySelector('.seconds');
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        if (clock) {
+            var daysSpan = clock.querySelector('.days');
+            var hoursSpan = clock.querySelector('.hours');
+            var minutesSpan = clock.querySelector('.minutes');
+            var secondsSpan = clock.querySelector('.seconds');
 
-    //     function updateClock() {
-    //         var t = getTimeRemaining(endtime);
+            function updateClock() {
+                var t = getTimeRemaining(endtime);
 
-    //         daysSpan.innerHTML = ('0' + t.days).slice(-2);
-    //         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    //         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    //         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+                daysSpan.innerHTML = ('0' + t.days).slice(-2);
+                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-    //         if (t.total <= 0) {
-    //             clearInterval(timeinterval);
-    //         }
-    //     }
+                if (t.total <= 0) {
+                    clearInterval(timeinterval);
+                }
+            }
 
-    //     updateClock();
-    //     var timeinterval = setInterval(updateClock, 1000);
-    // }
-    // //var deadline = 'July 17 2017 07:59:59 GMT-0700';
-    // var deadline = new Date(Date.parse(new Date()) + 100 * 24 * 60 * 60 * 1000); // sets 15 day countdown
-    // initializeClock('clockdiv', deadline);
+            updateClock();
+            var timeinterval = setInterval(updateClock, 1000);
+        }
+    }
+    //var deadline = 'July 17 2017 07:59:59 GMT-0700';
+    var deadline = new Date(Date.parse(new Date()) + 100 * 24 * 60 * 60 * 1000); // sets 15 day countdown
+    initializeClock('clockdiv', deadline);
 
     $('#submit-results').click(function() {
         if (window.location.host === 'site232.digitalskillsacademy.me') {
