078582eaafdee4ada9648fbbc19af00894e2ae31
unseen1980 <koutsiaris@gmail.com>
2017-01-21 12:54:14 +0000

    animations, eshop, misc changes

--- UXD_Poc_A3/scripts/app.js
+++ UXD_Poc_A3/scripts/app.js
@@ -262,4 +262,17 @@ $(function() {
             }
         }
     }
+
+    var quotes = $(".moto");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(15000)
+            .fadeOut(2000, showNextQuote)
+    }
+
+    showNextQuote();
 });
