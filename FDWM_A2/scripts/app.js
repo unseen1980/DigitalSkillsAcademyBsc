271ba3cfc86a69e6b8828672517e998399af6ac1
unseen1980 <koutsiaris@gmail.com>
2017-02-01 21:47:06 +0000

    styling fixes

--- FDWM_A2/scripts/app.js
+++ FDWM_A2/scripts/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         '<li class="hidden-md hidden-lg breadcrumb-mobile-style"><a href="#">UK</a></li>' +
         '<li class="hidden-md hidden-lg breadcrumb-mobile-style"><a href="#">world</a></li>' +
         '<li class="hidden-md hidden-lg breadcrumb-mobile-style"><a href="#">sport</a></li>' +
-        '<li class="hidden-md hidden-lg breadcrumb-mobile-style"><a href="#">football</a></li>' +
+        '<li class="hidden-md hidden-lg hidden-xs breadcrumb-mobile-style"><a href="#">football</a></li>' +
         '<li class="hidden-md hidden-lg hidden-xs breadcrumb-mobile-style"><a href="#">opinion</a></li>' +
         '<li class="hidden-md hidden-lg hidden-xs breadcrumb-mobile-style"><a href="#">culture</a></li>' +
         '<li class="hidden-md hidden-lg breadcrumb-mobile-style navbar-right"><a href="#"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span><span class="hidden-lg">all</span></a></li>'
