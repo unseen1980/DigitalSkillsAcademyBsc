22f88221d25d4fae0f8559b1e20842bf9e06fce6
unseen1980 <koutsiaris@gmail.com>
2017-02-04 14:24:39 +0000

    add comments

--- FDWM_A2/scripts/app.js
+++ FDWM_A2/scripts/app.js
@@ -1,3 +1,8 @@
+// * Author: Christos Koutsiaris
+// * Assignment: Minor Project – The Guardian tech page
+// * Student ID: STU-00001219
+// * Date: 2017/02/04
+// * Code: BScHn2016B_FDWM_A2
 $(document).ready(function() {
     $("[data-toggle=popover]").popover({ trigger: "focus" });
 });
