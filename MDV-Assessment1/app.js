728e58c5c0a5120709ed8552e3f6bdce14c74cae
unseen1980 <koutsiaris@gmail.com>
2017-04-02 22:00:57 +0100

    add proper json

--- MDV-Assessment1/app.js
+++ MDV-Assessment1/app.js
@@ -1,206 +1,53 @@
 $(document).ready(function() {
-    var packages = {
-        "Europe": {
-            "Greece": [{
-                    "name": "Athens",
-                    "days": 5,
-                    "price": 200,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Crete",
-                    "days": 5,
-                    "price": 300,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Corfu",
-                    "days": 5,
-                    "price": 400,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                }
-            ],
-            "Spain": [{
-                    "name": "Madrid",
-                    "days": 5,
-                    "price": 200,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Valencia",
-                    "days": 5,
-                    "price": 300,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Majorca",
-                    "days": 5,
-                    "price": 400,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                }
-            ],
-            "Italy": [{
-                    "name": "Rome",
-                    "days": 5,
-                    "price": 200,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Venice",
-                    "days": 5,
-                    "price": 300,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Milano",
-                    "days": 5,
-                    "price": 400,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                }
-            ],
-            "Portugal": [{
-                    "name": "Lisbon",
-                    "days": 5,
-                    "price": 200,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Faro",
-                    "days": 5,
-                    "price": 300,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                },
-                {
-                    "name": "Azores",
-                    "days": 5,
-                    "price": 400,
-                    "hotelName": "",
-                    "airline": "",
-                    "images": [],
-                    "desc": ""
-                }
-            ]
-        },
-        "United-States": {
-            "California": [{
-                    "name": "Santa Monica",
-                    "days": 5,
-                    "price": 200
-                },
-                {
-                    "name": "Hollywood",
-                    "days": 5,
-                    "price": 300
-                },
-                {
-                    "name": "Los Angeles",
-                    "days": 5,
-                    "price": 400
-                }
-            ]
-        },
-        "Asia": {
-            "China": [{
-                    "name": "Beijing",
-                    "days": 5,
-                    "price": 200
-                },
-                {
-                    "name": "Shenzen",
-                    "days": 5,
-                    "price": 300
-                }
-            ]
-        },
-        "Africa": {
-            "South-Africa": [{
-                    "name": "Cape Town",
-                    "days": 5,
-                    "price": 200
-                },
-                {
-                    "name": "Port Elizabeth",
-                    "days": 5,
-                    "price": 300
-                },
-                {
-                    "name": "East London",
-                    "days": 5,
-                    "price": 400
-                }
-            ]
-        }
-    };
+    $.getJSON('http://site232.digitalskillsacademy.me/MDV-Assessment1/response.json')
+        .done(function(data) {
+            var packages = data,
+                packagesList = $('#packages-list');
 
-    var packagesList = $('#packages-list');
-
-    for (var key in packages) {
-        if (packages.hasOwnProperty(key)) {
-            packagesList.append('<li data-corners="false" id="' + key + '"></li>').listview().listview('refresh');
-            for (var keyCountry in packages[key]) {
-                if (packages[key].hasOwnProperty(keyCountry)) {
-                    $('#' + key).append('<div data-role="collapsible"><h2 class="reset-margin">' +
-                        keyCountry +
-                        '</h2><ul  data-role="listview" data-shadow="false" data-inset="true" id="' +
-                        keyCountry +
-                        '"></ul></div>').collapsibleset().trigger('create');
-                    packages[key][keyCountry].forEach(function(city) {
-                        $('#' + keyCountry).append('<li><a href="#' + city.name + '">' + city.name + '</a></li>').listview().listview('refresh');
-                        var pageContent = '<div data-role="page" id=' + city.name + '>' +
-                            '<div data-role="header">' +
-                            '<h1>' + city.name + '</h1>' +
-                            '</div>' +
-                            '<div role="main" class="ui-content">' +
-                            '<a href="#main" class="ui-btn">back</a>' +
-                            '</div>' +
-                            '<div data-role="footer">' +
-                            '<h4>Powered by Jquery Mobile</h4>' +
-                            '</div>' +
-                            '</div>';
-                        $('body').append(pageContent);
-                    })
+            for (var key in packages) {
+                if (packages.hasOwnProperty(key)) {
+                    packagesList.append('<li data-corners="false" id="' + key + '"></li>').listview().listview('refresh');
+                    for (var keyCountry in packages[key]) {
+                        if (packages[key].hasOwnProperty(keyCountry)) {
+                            $('#' + key).append('<div data-role="collapsible"><h2 >' +
+                                keyCountry +
+                                '</h2><ul  data-role="listview" data-shadow="false" data-inset="true" id="' +
+                                keyCountry +
+                                '"></ul></div>').collapsibleset().trigger('create');
+                            packages[key][keyCountry].forEach(function(city) {
+                                $('#' + keyCountry).append('<li><a data-transition="slide" href="#' + city.name + '">' + city.name + '</a></li>').listview().listview('refresh');
+                                var pageContent = '<div data-role="page" id=' + city.name + '>' +
+                                    '<div data-role="header">' +
+                                    '<h1>' + city.name + '</h1>' +
+                                    '</div>' +
+                                    '<div role="main" class="ui-content">' +
+                                    '<img class="center-image" src="' + city.images[0] + '">' +
+                                    '<ul class="no-bullets">' +
+                                    '<li><b>Airline</b> - ' + city.airline + '</li>' +
+                                    '<li><b>Hotel</b> - ' + city.hotelName + '</li>' +
+                                    '<li><b>Departure</b> - ' + city.departure + '</li>' +
+                                    '<li><b>Duration</b> - ' + city.nights + ' nights</li>' +
+                                    '<li><b>Price</b> - ' + city.price + '€</li></ul>' +
+                                    '<p>' + city.desc + '</p>' +
+                                    '<a data-transition="slide" data-direction="reverse" href="#main" class="ui-btn ui-icon-back ui-btn-icon-left">Back</a>' +
+                                    '</div>' +
+                                    '<div data-role="footer" data-position="fixed">' +
+                                    '<h4>Digital Skills Travel 2017</h4>' +
+                                    '</div>' +
+                                    '</div>';
+                                $('body').append(pageContent);
+                            })
+                        }
+                    }
                 }
             }
-        }
-    }
 
-    packagesList.listview({
-        autodividers: true,
-        autodividersSelector: function(li) {
-            return li.attr('id');
-        }
-    }).listview("refresh");
+            packagesList.listview({
+                autodividers: true,
+                autodividersSelector: function(li) {
+                    return li.attr('id');
+                }
+            }).listview("refresh");
+        });
+
 });
