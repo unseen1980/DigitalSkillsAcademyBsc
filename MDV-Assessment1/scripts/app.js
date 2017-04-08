dd672df6cd9f98cfe2b95efa33b09576e7cdd1f8
unseen1980 <koutsiaris@gmail.com>
2017-04-08 10:43:25 +0100

    folder structure

--- MDV-Assessment1/scripts/app.js
+++ MDV-Assessment1/scripts/app.js
@@ -1,3 +1,9 @@
+// * Author: Christos Koutsiaris
+// * Assessment 1 -  Mobile jQuery
+// * Student ID: STU-00001219
+// * Date: 2017/04/08
+// * Code: BScHn16B_MDV_A1
+
 $(document).ready(function() {
     // Loader widget toggle 
     var loader = {
